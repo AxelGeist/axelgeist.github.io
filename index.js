326ce3c6c7e58c6974b7ec53ff615bb03100d07c
AxelGeist <geist.axel@gmail.com>
2026-07-30 23:17:04 +0200

    Improve hero and section navigation

--- index.js
+++ index.js
@@ -26,39 +26,6 @@ const initializeSkillsCarousel = () => {
 
 initializeSkillsCarousel();
 
-// Welcome Text: typing effect
-let t = 0 , j = 0;
-let array = document.getElementById("typingEffect").innerHTML.split(",");
-
-const split_word = () => {
-  if (j < array[t].length) {
-    document.getElementById("holder").innerHTML += array[t].charAt(j);
-    j++;
-    setTimeout(split_word, 110); // Time gap between each letter
-  } else {
-    setTimeout(delete_word, 700); // Pause after every word
-  }
-};
-
-const delete_word = () => {
-  if (j >= 0) {
-    let del = array[t].toString().slice(0, j);
-    document.getElementById("holder").innerHTML = del;
-    j--;
-    setTimeout(delete_word, 40);  // Deletion speed
-  } 
-  else {
-      t++;
-    if (t >= array.length){ 
-      t = 0;
-    }
-    setTimeout(split_word, 110);  // Wait for next word
-  }
-};
-
-split_word();
-
-
 // Navbar: smooth color transition
 
 window.addEventListener('scroll', function() {
